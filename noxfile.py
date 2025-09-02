8b8c786f59c0577322d0bca2f01952e118f69b8b
Jessica Smith <12jessicasmith34@gmail.com>
2025-09-02 11:53:03 -0500

    add new env for noxfile, add back 3.12 and 3.13 to matrix

--- noxfile.py
+++ noxfile.py
@@ -11,3 +11,10 @@ nox.options.default_venv_backend = "uv|virtualenv"
 @nox.session(python=["3.11", "3.12", "3.13"])
 def tests(session: "Session"):
     session.run("uv", "run", "--active", "--reinstall-package", "hassette", "pytest", external=True)
+
+
+@nox.session(python=["3.11", "3.12", "3.13"])
+def tests_no_ha(session: "Session"):
+    session.run(
+        "uv", "run", "--active", "--reinstall-package", "hassette", "pytest", "-m", "not requires_ha", external=True
+    )
