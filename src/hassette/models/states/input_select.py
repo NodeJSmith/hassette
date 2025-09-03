64302c2bc74a783c2fcf4f25230b246fd4d45f76
Jessica Smith <12jessicasmith34@gmail.com>
2025-09-03 06:22:58 -0500

    add missing options to InputSelect

--- src/hassette/models/states/input_select.py
+++ src/hassette/models/states/input_select.py
@@ -1,4 +1,4 @@
-from typing import Literal
+from typing import Any, Literal
 
 from pydantic import Field
 
@@ -8,7 +8,11 @@ from .base import AttributesBase, StringBaseState
 class InputSelectState(StringBaseState):
     class Attributes(AttributesBase):
         editable: bool | None = Field(default=None)
+        options: list[str] = Field(default_factory=list)
 
     domain: Literal["input_select"]
 
     attributes: Attributes | None = Field(default=None)
+
+    def model_post_init(self, context: Any) -> None:
+        return super().model_post_init(context)
