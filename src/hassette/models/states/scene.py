978a2e9b70b99beb48db1c41362069441fc800a8
Jessica Smith <12jessicasmith34@gmail.com>
2025-09-05 06:45:14 -0500

    fix last few states that didn't inherit from a typed base state

--- src/hassette/models/states/scene.py
+++ src/hassette/models/states/scene.py
@@ -1,12 +1,11 @@
 from typing import Literal
 
 from pydantic import Field
-from whenever import Instant
 
-from .base import AttributesBase, BaseState
+from .base import AttributesBase, InstantBaseState
 
 
-class SceneState(BaseState[Instant | None]):
+class SceneState(InstantBaseState):
     class Attributes(AttributesBase):
         entity_id: list[str] | None = Field(default=None)
         id: str | None = Field(default=None)
