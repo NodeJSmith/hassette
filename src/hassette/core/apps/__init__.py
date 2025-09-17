250fd50e4d8a438e4809f29262b3c74fe31caf3c
Jessica Smith <12jessicasmith34@gmail.com>
2025-09-17 11:07:17 -0500

    add @only decorator to easily run a single app without changing config

--- src/hassette/core/apps/__init__.py
+++ src/hassette/core/apps/__init__.py
@@ -1,4 +1,4 @@
-from .app import App, AppSync
+from .app import App, AppSync, only
 from .app_config import AppConfig, AppConfigT
 
 __all__ = [
@@ -6,4 +6,5 @@ __all__ = [
     "AppConfig",
     "AppConfigT",
     "AppSync",
+    "only",
 ]
