626d5b2ded350c7010a05232fcc265e6815188d0
Jessica Smith <12jessicasmith34@gmail.com>
2025-09-23 09:21:25 -0500

    remove redundant call

--- tests/test_service_watcher.py
+++ tests/test_service_watcher.py
@@ -32,11 +32,9 @@ def get_dummy_service(called: dict[str, int], hassette) -> Service:
 async def test_restart_service_cancels_then_starts(get_service_watcher_mock: _ServiceWatcher):
     called = {"cancel": 0, "start": 0}
 
-    get_service_watcher_mock.hassette._resources["_Dummy"] = get_dummy_service(
+    get_service_watcher_mock.hassette._resources["_Dummy"] = svc = get_dummy_service(
         called, get_service_watcher_mock.hassette
     )
-
-    svc = get_dummy_service(called, get_service_watcher_mock)
 
     event = create_service_status_event(
         resource_name=svc.class_name,
