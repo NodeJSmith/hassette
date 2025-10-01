6976900ab6f9527d955e6124aaab62ba31956e33
Jessica Smith <12jessicasmith34@gmail.com>
2025-09-30 19:34:13 -0500

    refactor(health_service.py): restructure health service startup and shutdown methods for better clarity and maintainability
    This change introduces a dedicated `startup` method to handle the initialization of the health HTTP server and a `shutdown` method to manage cleanup, improving code organization and readability.

--- src/hassette/core/health_service.py
+++ src/hassette/core/health_service.py
@@ -8,6 +8,17 @@ from hassette.core.enums import ResourceStatus
 if typing.TYPE_CHECKING:
     from hassette.core.core import Hassette
 
+_T = typing.TypeVar("_T")
+
+
+# subclass to prevent the weird UnboundLocalError we get from aiohttp
+# i think it's due to pytest but i'm tired of trying to figure it out
+# that's why you don't use frame inspections
+class MyAppKey(web.AppKey[_T]):
+    def __init__(self, name: str, t: type[_T]):
+        self._name = __name__ + "." + name
+        self._t = t
+
 
 class _HealthService(Service):
     """Tiny HTTP server exposing /healthz for container healthchecks."""
@@ -25,22 +36,11 @@ class _HealthService(Service):
             return
 
         try:
-            app = web.Application()
-            hassette_key = web.AppKey["Hassette"]("hassette")
-            app[hassette_key] = self.hassette
-            app.router.add_get("/healthz", self._handle_health)
+            async with self.starting():
+                await self.startup()
 
-            self._runner = web.AppRunner(app)
-            await self._runner.setup()
-            site = web.TCPSite(self._runner, self.host, self.port)
-            await site.start()
-
-            self.logger.info("Health service listening on %s:%s", self.host, self.port)
-
-            # don't send start event until server is running
-            await self.handle_start()
             # Just idle until cancelled
-            await self.hassette._shutdown_event.wait()
+            await self.hassette.shutdown_event.wait()
         except OSError as e:
             error_no = e.errno if hasattr(e, "errno") else type(e)
             self.logger.error("Health service failed to start: %s (errno=%s)", e, error_no)
@@ -51,6 +51,26 @@ class _HealthService(Service):
             raise
         finally:
             await self._cleanup()
+
+    async def startup(self):
+        """Start the health HTTP server."""
+        self.logger.debug("Waiting for Hassette ready event")
+        await self.hassette.ready_event.wait()
+        app = web.Application()
+        hassette_key = MyAppKey[_HealthService]("health_service", _HealthService)
+        app[hassette_key] = self
+        app.router.add_get("/healthz", self._handle_health)
+
+        self._runner = web.AppRunner(app)
+        await self._runner.setup()
+        site = web.TCPSite(self._runner, self.host, self.port)
+        await site.start()
+
+        self.logger.info("Health service listening on %s:%s", self.host, self.port)
+
+    async def shutdown(self, *args, **kwargs) -> None:
+        await self._cleanup()
+        return await super().shutdown(*args, **kwargs)
 
     async def _cleanup(self) -> None:
         if self._runner:
