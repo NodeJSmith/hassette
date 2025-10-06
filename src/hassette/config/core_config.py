32b77ce0a533f51c4834a07ff001eacbb5258150
Jessica Smith <8505845+NodeJSmith@users.noreply.github.com>
2025-10-06 11:31:37 -0500

    Implement default log level settings in config
    Set default log levels for specified fields if not already set.

--- src/hassette/config/core_config.py
+++ src/hassette/config/core_config.py
@@ -275,6 +275,14 @@ class HassetteConfig(HassetteBaseSettings):
 
     @model_validator(mode="after")
     def show_details_at_startup(self) -> "HassetteConfig":
+        # TODO: there has to be a cleaner way to do this
+        for field in ("app_handler_log_level", "bus_service_log_level", "scheduler_service_log_level",
+                     "health_service_log_level", "websocket_log_level", "service_watcher_log_level",
+                     "file_watcher_log_level"):
+            if field not in self.model_fields_set:
+                LOGGER.debug("Setting default log level for %s to %s", field, self.log_level)
+                setattr(self, field, self.log_level)
+
         if "debugpy" in sys.modules:
             if "dev_mode" not in self.model_fields_set:
                 LOGGER.warning("Developer mode enabled via debugpy")
