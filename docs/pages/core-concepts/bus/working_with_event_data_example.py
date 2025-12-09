4aae3f689f405a03a3ffc040d827a3611e32157a
Jessica Smith <12jessicasmith34@gmail.com>
2025-12-09 16:23:18 -0600

    fix example

--- docs/pages/core-concepts/bus/working_with_event_data_example.py
+++ docs/pages/core-concepts/bus/working_with_event_data_example.py
@@ -6,19 +6,15 @@ from hassette import dependencies as D
 
 class WorkingWithEventDataExample(App):
     async def on_light_change(
-        self,
-        new_state: D.StateNew[states.LightState],
-        old_state: D.MaybeStateOld[states.LightState],
-        new_state_value: D.StateValueNew[str],
-        old_state_value: D.MaybeStateValueOld[str],
+        self, new_state: D.StateNew[states.LightState], old_state: D.MaybeStateOld[states.LightState]
     ) -> None:
-        self.logger.info("%s changed from %s to %s", new_state.entity_id, old_state_value, new_state_value)
+        self.logger.info("%s changed from %s to %s", new_state.entity_id)
 
         reveal_type(new_state)  # LightState
 
         reveal_type(old_state)  # LightState | None
 
-        reveal_type(new_state_value)  # str
-        reveal_type(old_state_value)  # str | None
+        new_state_value = new_state.value
+        old_state_value = old_state.value if old_state is not None else None
 
         self.logger.info("%s changed from %s to %s", new_state.entity_id, old_state_value, new_state_value)
