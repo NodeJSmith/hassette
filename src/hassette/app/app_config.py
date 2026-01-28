7ed50c7d3b675db4fb6ad0ed1abba0d7a9418bf0
Jessica Smith <12jessicasmith34@gmail.com>
2026-01-27 22:09:05 -0600

    add basic log level validation to AppConfig

--- src/hassette/app/app_config.py
+++ src/hassette/app/app_config.py
@@ -1,5 +1,7 @@
+import logging
 from typing import TypeVar
 
+from pydantic import field_validator
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 from hassette.types.types import LOG_LEVELS
@@ -21,6 +23,18 @@ class AppConfig(BaseSettings):
 
     log_level: LOG_LEVELS = "INFO"
     """Log level for the app instance. Defaults to 'INFO'."""
+
+    @field_validator("log_level")
+    @classmethod
+    def validate_log_level(cls, v):
+        if not isinstance(v, str):
+            raise ValueError(f"Log level must be a string, got {type(v)}")
+
+        v = v.upper()
+
+        if not hasattr(logging, v):
+            raise ValueError(f"Invalid log level: {v}")
+        return v
 
 
 AppConfigT = TypeVar("AppConfigT", bound=AppConfig)
