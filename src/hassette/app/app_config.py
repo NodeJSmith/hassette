b71a7183f6e43e7231ea2f6a8b8f9521b6b85d1a
Jessica Smith <12jessicasmith34@gmail.com>
2026-01-31 22:41:16 -0600

    fix some comments/docstrings in AppConfig

--- src/hassette/app/app_config.py
+++ src/hassette/app/app_config.py
@@ -9,11 +9,11 @@ from hassette.types.types import LOG_LEVEL_TYPE
 class AppConfig(BaseSettings):
     """Base configuration class for applications in the Hassette framework.
 
-    This default class does not define any fields, allowing anyone who prefers to not use
-    this functionality to ignore it. It also allows all extras, so arbitrary additional
-    configuration data can be passed without needing to define a custom subclass.
+    This default class allows all extras, so arbitrary additional configuration data
+    can be passed without needing to define a custom subclass, at the cost of type safety.
 
-    Fields can be set on subclasses and extra can be overriden by assigning a new value to `model_config`."""
+    Fields can be set on subclasses and extra can be overriden by assigning a new value to `model_config`.
+    """
 
     model_config = SettingsConfigDict(
         extra="allow", arbitrary_types_allowed=True, env_file=ENV_FILE_LOCATIONS, env_ignore_empty=True
@@ -23,4 +23,4 @@ class AppConfig(BaseSettings):
     """Name for the instance of the app."""
 
     log_level: LOG_LEVEL_TYPE = Field(default_factory=log_level_default_factory)
-    """Log level for the app instance. Defaults to Hassette default if not provided."""
+    """Log level for the app instance. Defaults to INFO if not provided."""
