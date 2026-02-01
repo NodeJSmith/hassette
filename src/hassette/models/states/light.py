ac7a79b27cf726c59a2ab36d6243d61fb9c5eaf3
Jessica Smith <12jessicasmith34@gmail.com>
2026-01-31 23:23:58 -0600

    improve light attributes

--- src/hassette/models/states/light.py
+++ src/hassette/models/states/light.py
@@ -1,4 +1,4 @@
-from typing import Any, Literal
+from typing import Literal
 
 from pydantic import Field
 
@@ -6,20 +6,47 @@ from .base import AttributesBase, StringBaseState
 
 
 class LightAttributes(AttributesBase):
-    min_color_temp_kelvin: int | float | None = Field(default=None)
-    max_color_temp_kelvin: int | float | None = Field(default=None)
-    min_mireds: int | float | None = Field(default=None)
-    max_mireds: int | float | None = Field(default=None)
     effect_list: list[str] | None = Field(default=None)
-    supported_color_modes: list[str] | None = Field(default=None)
-    effect: Any | None = Field(default=None)
+    """The list of supported effects."""
+
+    supported_color_modes: set[str] | None = Field(default=None)
+    """Flag supported color modes."""
+
+    effect: str | None = Field(default=None)
+    """The current effect."""
+
     color_mode: str | None = Field(default=None)
-    brightness: int | float | None = Field(default=None)
-    color_temp_kelvin: int | float | None = Field(default=None)
-    color_temp: int | float | None = Field(default=None)
-    hs_color: list[float] | None = Field(default=None)
-    rgb_color: list[int] | None = Field(default=None)
+    """The color mode of the light with backwards compatibility."""
+
+    brightness: int | None = Field(default=None, gt=-1, lt=256)
+    """The brightness of this light between 0..255."""
+
+    color_temp_kelvin: int | None = Field(default=None)
+    """The CT color value in Kelvin."""
+
+    min_color_temp_kelvin: int | None = Field(default=None)
+    """The warmest color_temp_kelvin that this light supports."""
+
+    max_color_temp_kelvin: int | None = Field(default=None)
+    """The coldest color_temp_kelvin that this light supports."""
+
+    hs_color: tuple[float, float] | None = Field(default=None)
+    """The hue and saturation color value."""
+
+    rgb_color: tuple[int, int, int] | None = Field(default=None)
+    """The rgb color value."""
+
     xy_color: list[float] | None = Field(default=None)
+    """The x and y color value."""
+
+    min_mireds: int | None = Field(default=None, deprecated=True)
+    """Deprecated: The coldest color_temp that this light supports."""
+
+    max_mireds: int | None = Field(default=None, deprecated=True)
+    """Deprecated: The warmest color_temp that this light supports."""
+
+    color_temp: int | None = Field(default=None, deprecated=True)
+    """Deprecated: The CT color value in mireds."""
 
 
 class LightState(StringBaseState):
